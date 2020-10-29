ad18641be8071262dc91e1fc821b757b4dc4785a
jaime7981 <jmfernandez1@miuandes.cl>
2020-10-29 18:20:14 -0300

    primeros pasos

--- HuberEats.py
+++ HuberEats.py
@@ -1,9 +1,38 @@
-import psycopg2 as sv
+import psycopg2 as svpg
+import tabulate
 
-con = sv.connect(database="grupo6", 
+con = svpg.connect(database="grupo6", 
                  user="grupo6", 
                  password="99sFKQ", 
                  host="201.238.213.114", 
                  port="54321")
 
 print("Conexion exitosa")
+
+#SELECT * FROM categorias  // querry de ejemplo
+#con.commit()  // Actualiza los cambios en la base de datos, 
+#                   asi como los CREATE TABLE o UPDATES, etc
+
+
+def InsertQuerry(text):
+    cur = con.cursor()
+    cur.execute(text)
+    request = cur.fetchall()
+    headers = []
+#    for a in request:
+#        headers.append(a[0])
+#    print(tabulate(request, headers, tablefmt="psql"))
+    print(request)
+
+flag = True
+
+while flag:
+    opcion = input("Ingrese opcion\n1) Querry\n2)Exit\n")
+    if opcion == "1":
+        querry = input("Ingresar querry: ")
+        InsertQuerry(querry)
+    if opcion == "2":
+        flag = False
+
+con.close()
+
